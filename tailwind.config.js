1e712e4c8ed196f66ef96440398ff5a0d9cc0340
simutis <54304022+SimasG@users.noreply.github.com>
2022-02-20 10:56:12 +0200

    progressed with the mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
       fontFamily: {
         body: ["Manrope"],
       },
+      colors: {
+        white: "#ffffff",
+        "very-pale-blue": "#fafbff",
+        "light-grayish-blue-slider": "#eaeefb",
+        "light-grayish-blue-toggle": "#cdd7ee",
+        "grayish-blue": "#858fad",
+        "dark-desaturated-blue": "#293356",
+      },
     },
   },
   plugins: [],
